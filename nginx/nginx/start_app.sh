d36901051c71a2d9c1838705c720067adb543472
MilanFun <ptukha.aiu@phystech.edu>
2023-03-09 21:38:09 +0300

    [Script] rework start script

--- nginx/nginx/start_app.sh
+++ nginx/nginx/start_app.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+set -e
+# Build
+cd ..
+mvn clean install > /dev/null
+echo "Build finished with: $?"
+
+find_free_port() {
+  read LOWERPORT UPPERPORT < /proc/sys/net/ipv4/ip_local_port_range
+  while :
+  do
+    FREE_PORT=$(shuf -i $LOWERPORT-$UPPERPORT -n 1)
+    ss -lpn | grep -q ":$PORT " || break
+  done
+}
+
+# Start 3 services on different port
 for i in 1 2 3
 do
-	java -jar nginx-0.0.${i}-SNAPSHOT.jar &
+  find_free_port
+  echo "Starting service on port: $FREE_PORT"
+	java -jar target/nginx-0.0.3-SNAPSHOT.jar --server.port=$FREE_PORT > /dev/null &
 	sleep 5
 done
